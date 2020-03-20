f059677106b51e023f06761406fd4a2cacb44864
irmir10 <60602674+irmir10@users.noreply.github.com>
2020-03-20 03:12:19 +0200

    Add files via upload

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import json
+import hashlib
 
 with open('db.json', 'w') as db:
     json.dump({'admin': '123'}, db)
@@ -11,7 +12,7 @@ def clear(event):
 
 
 def register():
-    user = {username.get(): password.get()}
+    user = {username.get(): hashlib.sha256(password.get().encode()).hexdigest()}
 
     with open('db.json') as db:
         database = json.load(db)
@@ -26,7 +27,7 @@ def register():
 
 
 def login():
-    user = {username.get(): password.get()}
+    user = {username.get(): hashlib.sha256(password.get().encode()).hexdigest()}
     if not username.get() or not password.get():
         return error.configure(text='Error occurred please try again.')
     with open('db.json') as db:
@@ -42,27 +43,26 @@ def login():
 window = Tk()
 window.title('Login')
 window.resizable(width=False, height=False)
+window.configure(bg='lightblue')
 
-lbl = Label(window, text='Welcome', font=('impact', 20)).grid(row=0, columnspan=2)
-
-lbl_username = Label(window, text='Username:').grid(row=1, column=0)
+lbl = Label(window, text='Welcome', font=('impact', 20), bg='lightblue')
+lbl_username = Label(window, text='Username:')
 username = Entry(window)
-username.grid(row=1, column=1, padx=(0, 10))
 username.bind('<FocusIn>', clear)
-
-lbl_password = Label(window, text='Password:').grid(row=2, column=0)
-password = Entry(window)
-password.grid(row=2, column=1, padx=(0, 10))
+lbl_password = Label(window, text='Password:')
+password = Entry(window, show="*")
 password.bind('<FocusIn>', clear)
-
-
 login = Button(window, text='Login', bg='gray', command=login)
-login.grid(row=4, columnspan=2, padx=(0, 100), pady=(10, 10))
-
 register = Button(window, text='register', bg='gray', command=register)
-register.grid(row=4, columnspan=2, padx=(100, 0))
+error = Label(window, text='', font=('impact', 10), bg='lightblue')
 
-error = Label(window, text='', font=('impact', 10))
+lbl.grid(row=0, columnspan=2)
+lbl_username.grid(row=1, column=0)
+username.grid(row=1, column=1, padx=(0, 10))
+lbl_password.grid(row=2, column=0)
+password.grid(row=2, column=1, padx=(0, 10))
+login.grid(row=4, columnspan=2, padx=(0, 100), pady=(10, 10))
+register.grid(row=4, columnspan=2, padx=(100, 0))
 error.grid(row=5, columnspan=2)
 
 window.mainloop()
